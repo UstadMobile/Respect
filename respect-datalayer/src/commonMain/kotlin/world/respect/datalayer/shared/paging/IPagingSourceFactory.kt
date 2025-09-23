700ad04042a4e4109171ddb7bdce4be9b30f297f
Mike Dawson <mike@ustadmobile.com>
2025-09-22 19:34:55 -0700

    Add IPagingSourceFactory docs.

--- respect-datalayer/src/commonMain/kotlin/world/respect/datalayer/shared/paging/IPagingSourceFactory.kt
+++ respect-datalayer/src/commonMain/kotlin/world/respect/datalayer/shared/paging/IPagingSourceFactory.kt
@@ -2,6 +2,11 @@ package world.respect.datalayer.shared.paging
 
 import androidx.paging.PagingSource
 
+/**
+ * Interface that provides a PagingSource. This allows using stateful implementations where required
+ * e.g. RepositoryPagingSourceFactory which hosts a RemoteMediator whose lifecycle survives when
+ * the local PagingSource is invalidated (e.g. when it's updated by remote data).
+ */
 fun interface IPagingSourceFactory<Key: Any, Value: Any> {
 
     operator fun invoke(): PagingSource<Key, Value>

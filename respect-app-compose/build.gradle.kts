1ab2b32974704227905b3d18d1b69e76d3e7b42c
Mike Dawson <mike@ustadmobile.com>
2025-09-24 12:14:39 -0700

    Bump version

--- respect-app-compose/build.gradle.kts
+++ respect-app-compose/build.gradle.kts
@@ -163,8 +163,8 @@ android {
         applicationId = "world.respect.app"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         for(propName in ACRA_PROP_NAMES) {
             buildConfigField(

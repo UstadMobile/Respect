9fb1f8f54fa73d625c804287bf2a3606195463f9
Mike Dawson <mike@ustadmobile.com>
2025-10-06 10:10:29 -0400

    Move Maestro flows into .maestro to follow convention. Update CI scripts.

--- ci-run-maestro.sh
+++ ci-run-maestro.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Environment variables:
-# TESTCONTROLLER_PORT - port that the testcontroller will run on
+# Script used on CI (Continuous Integration - eg Jenkins) to run Maestro end to end tests (see 
+# .maestro for test flows
 
 SCRIPTDIR=$(realpath $(dirname $BASH_SOURCE))
+
+# Root directory for TestServerController to use (each server will get its own sub directory)
+# TestServerController will create the directory automatically.
+TESTSERVERCONTROLLER_BASEDIR="$SCRIPTDIR/build/testservercontroller/workspace"
 
 if [ "$TESTCONTROLLER_PORT" == "" ]; then
     TESTCONTROLLER_PORT=8094
@@ -11,7 +15,7 @@ fi
 
 if [ "$TESTCONTROLLER_URL" == "" ]; then
     TESTCONTROLLER_URL="http://$(hostname -I | xargs):$TESTCONTROLLER_PORT/"
-    echo "run-maestro-ci: ATTN: TESTCONTROLLER_URL not set. Setting to $TESTCONTROLLER_URL (this might not be correct)"
+    echo "ci-run-maestro: ATTN: TESTCONTROLLER_URL not set. Setting to $TESTCONTROLLER_URL (this might not be correct)"
 fi
 
 if [ "$TEST_LEARNINGSPACE_PORTRANGE" == "" ]; then
@@ -20,12 +24,12 @@ fi
 
 function cleanup() {
     if [ "$TESTCONTROLLER_PID" != "" ]; then
-        echo "run-maestro-ci: note 'No instance for key AttributeKey: KOIN_SCOPE' can be safely ignored"
-        echo "run-maestro-ci: Stopping TestServerController"
+        echo "ci-run-maestro: note 'No instance for key AttributeKey: KOIN_SCOPE' can be safely ignored"
+        echo "ci-run-maestro: Stopping TestServerController"
         wget -qO- "${TESTCONTROLLER_URL}shutdown"
         sleep 10
         if [ -d "/proc/$PID" ]; then
-            echo "run-maestro-ci: calling kill just in case (no such process error can be ignored)"
+            echo "ci-run-maestro: calling kill just in case (no such process error can be ignored)"
             kill $TESTCONTROLLER_PID
         fi
     fi
@@ -48,15 +52,16 @@ DIR_ADMIN_AUTH_HEADER="Basic $(printf '%s' $DIR_ADMIN_TO_ENCODE | base64)"
 
 $TESTCONTROLLER_BIN -P:ktor.deployment.port=$TESTCONTROLLER_PORT \
     -P:testservercontroller.portRange=$TEST_LEARNINGSPACE_PORTRANGE \
+    -P:testservercontroller.basedir=$TESTSERVERCONTROLLER_BASEDIR \
     -P:testservercontroller.env.DIR_ADMIN_AUTH=$DIR_ADMIN_AUTH_PASS \
     -P:ktor.deployment.shutdown.url=/shutdown \
     -P:testservercontroller.shutdown.url=/shutdown \
-    -P:testservercontroller.cmd="$SCRIPTDIR/run-server.sh" &
+    -P:testservercontroller.cmd="$SCRIPTDIR/ci-run-test-server.sh" &
 
 TESTCONTROLLER_PID=$!
 
 wait-port $TESTCONTROLLER_PORT
-echo "run-maestro-ci: TestServerController now running on port $TESTCONTROLLER_PORT (pid $TESTCONTROLLER_PID)"
+echo "ci-run-maestro: TestServerController now running on port $TESTCONTROLLER_PORT (pid $TESTCONTROLLER_PID)"
 
 # Can now run maestro - the TESTSERVERCONTROLLER url is known and we also know the admin auth to create a new school etc.
 
@@ -66,13 +71,22 @@ if [ ! -e build/results ]; then
     mkdir -p build/results
 fi
 
+if [ ! -e build/maestro/results ]; then
+    mkdir -p build/maestro/output
+fi
+
 maestro test \
   --env DIR_ADMIN_AUTH_PASS=$DIR_ADMIN_AUTH_PASS \
   --env TESTCONTROLLER_URL=$TESTCONTROLLER_URL \
   --env SCHOOL_ADMIN_PASSWORD=$SCHOOL_ADMIN_PASSWORD \
   --env DIR_ADMIN_AUTH_HEADER="$DIR_ADMIN_AUTH_HEADER" \
   --env SCHOOL_NAME=TestSchool \
-  e2e-tests/000_000_hello_world.yaml
+  --format=junit \
+  --test-output-dir=build/maestro/output \
+  --output=build/maestro/report.xml \
+  .maestro/flows/000_000_hello_world.yaml
+MAESTRO_STATUS=$?
 
+echo "ci-run-maestro: Maestro test completed. Workspaces are in $TESTSERVERCONTROLLER_BASEDIR"
 
-
+exit $MAESTRO_STATUS

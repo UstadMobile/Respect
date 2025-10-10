a83dceea9a6825d7f4cd44a111d8db2c42f3d9cc
pooja <pooja@ustadmobile.com>
2025-10-10 14:35:44 +0400

    Added mandatory field test

--- .maestro/flows/subflows/setDate.js
+++ .maestro/flows/subflows/setDate.js
@@ -4,12 +4,14 @@ const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Months are 
 const year = today.getFullYear();
 const currentDate = `${day}/${month}/${year}`; // Format: DD/MM/YYYY
 const lastYear = today.getFullYear() - 1;
-const pastYear = today.getFullYear() - 2;
+const pastYearC = today.getFullYear() - 10;  // C for child
+const pastYearP = today.getFullYear() - 30;  // P for Parent
 
 const yesterday = (today.getDate() - 1).toString().padStart(2, '0');
 const yesterdayDate = `${yesterday}/${month}/${year}`; // Format: DD/MM/YYYY
 const lastYearDate = `${day}/${month}/${lastYear}`; // Same day and month, but last year
-const pastYearDate = `${day}/${month}/${pastYear}`; // Same day and month, but 2 years ago
+const pastYearDateC = `${day}/${month}/${pastYearC}`; // Same day and month, but 10 years ago
+const pastYearDateP = `${day}/${month}/${pastYearP}`; // Same day and month, but 30 years ago
 
 const tomorrow = (today.getDate() + 1).toString().padStart(2, '0');
 const tomorrowDate = `${tomorrow}/${month}/${year}`; // Format: DD/MM/YYYY
@@ -29,4 +31,5 @@ output.currentDate = currentDate;
 output.currentTime = currentTime;
 output.testTime = testTime;
 output.lastYearDate = lastYearDate; // Last year’s date
-output.pastYearDate = pastYearDate;
+output.pastYearDateC = pastYearDateC;
+output.pastYearDateP = pastYearDateP;

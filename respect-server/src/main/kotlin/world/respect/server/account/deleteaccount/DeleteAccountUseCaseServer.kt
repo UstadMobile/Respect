e5cdd5b5ff5e39fe8af70ea669be6111ef94b8d2
Mandvii <mandvi.verma@sutara.org>
2025-12-02 13:57:06 +0530

    Refactor DeleteAccountUseCaseServer to remove unused dependency
    The `schoolDb` parameter was unused and has been removed from the `DeleteAccountUseCaseServer` constructor. The implementation now solely relies on the `schoolDataSource` to perform the delete operation.

--- respect-server/src/main/kotlin/world/respect/server/account/deleteaccount/DeleteAccountUseCaseServer.kt
+++ respect-server/src/main/kotlin/world/respect/server/account/deleteaccount/DeleteAccountUseCaseServer.kt
@@ -2,17 +2,14 @@ package world.respect.server.account.deleteaccount
 
 import world.respect.datalayer.AuthenticatedUserPrincipalId
 import world.respect.datalayer.SchoolDataSource
-import world.respect.datalayer.db.RespectSchoolDatabase
 import world.respect.shared.domain.account.deleteaccount.DeleteAccountUseCase
 
 class DeleteAccountUseCaseServer(
-    private val schoolDb: RespectSchoolDatabase,
     private val schoolDataSource: SchoolDataSource,
     private val authenticatedUser: AuthenticatedUserPrincipalId
 ) : DeleteAccountUseCase {
 
     override suspend fun invoke(): Boolean {
-        // Use the SchoolDataSource (not DAO)
         return schoolDataSource
             .personDataSource
             .delete(authenticatedUser.guid)

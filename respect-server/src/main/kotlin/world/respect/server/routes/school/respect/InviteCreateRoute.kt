1353fb2d7646f9cf74c4989619d8757b6848d106
Mike Dawson <mike@ustadmobile.com>
2026-01-27 15:14:43 +0400

    Remove old Invite model.

--- respect-server/src/main/kotlin/world/respect/server/routes/school/respect/InviteCreateRoute.kt
+++ respect-server/src/main/kotlin/world/respect/server/routes/school/respect/InviteCreateRoute.kt
@@ -1,12 +1,5 @@
 package world.respect.server.routes.school.respect
 
-import io.ktor.server.application.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import world.respect.datalayer.school.model.Invite
-import world.respect.shared.domain.account.invite.CreateInviteUseCase
-
 //fun Route.InviteCreateRoute(
 //    createInviteUseCase: (ApplicationCall) -> CreateInviteUseCase,
 //) {

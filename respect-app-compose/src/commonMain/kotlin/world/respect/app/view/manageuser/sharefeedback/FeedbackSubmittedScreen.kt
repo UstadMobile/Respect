ebdf205e7743ca2e21c120d1984761739dd04ca5
mandviverma <mandvi.verma@sutara.org>
2026-01-30 16:36:48 +0530

    feat: Display Zammad ticket ID on feedback submission screen
    Adds the Zammad ticket ID to the "Feedback Submitted" screen, making it visible to the user for reference. This ID is displayed in a new `Text` element and tagged for testing purposes.

--- respect-app-compose/src/commonMain/kotlin/world/respect/app/view/manageuser/sharefeedback/FeedbackSubmittedScreen.kt
+++ respect-app-compose/src/commonMain/kotlin/world/respect/app/view/manageuser/sharefeedback/FeedbackSubmittedScreen.kt
@@ -5,16 +5,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.skia.Color
 import world.respect.app.components.defaultItemPadding
 import world.respect.app.components.defaultScreenPadding
 import world.respect.shared.generated.resources.Res
@@ -47,6 +50,13 @@ fun FeedbackSubmittedScreen(
                 modifier = Modifier.defaultItemPadding(),
                 text = stringResource(Res.string.feedback_submitted_message),
                 textAlign = TextAlign.Center
+            )
+        }
+
+        item {
+            Text(
+                text = uiState.ticketId.toString(),
+                modifier = Modifier.testTag("zammad_case_id"),
             )
         }
 

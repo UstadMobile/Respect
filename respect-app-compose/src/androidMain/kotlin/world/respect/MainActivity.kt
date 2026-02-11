c53314d2a88412693adf4a928f7c8b13712e035e
mandviverma <mandvi.verma@sutara.org>
2026-02-11 13:00:21 +0530

    feat: Add Hindi translation for "Get Started" button

--- respect-app-compose/src/androidMain/kotlin/world/respect/MainActivity.kt
+++ respect-app-compose/src/androidMain/kotlin/world/respect/MainActivity.kt
@@ -1,12 +1,8 @@
 package world.respect
 
-import android.app.LocaleManager
-import android.os.Build
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.os.LocaleListCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -16,6 +12,7 @@ import org.koin.android.ext.android.getKoin
 import org.koin.android.scope.AndroidScopeComponent
 import org.koin.androidx.scope.activityScope
 import org.koin.core.scope.Scope
+import world.respect.app.app.App
 import world.respect.credentials.passkey.CreatePasskeyUseCaseAndroidChannelHost
 import world.respect.credentials.passkey.CreatePasskeyUseCaseProcessor
 import world.respect.credentials.passkey.GetCredentialUseCase
@@ -29,7 +26,6 @@ import world.respect.datalayer.respect.model.RespectSchoolDirectory
 import world.respect.shared.domain.biometric.BiometricAuthProcessor
 import world.respect.shared.domain.biometric.BiometricAuthUseCaseAndroidImpl
 import world.respect.view.app.AbstractAppActivity
-import kotlin.jvm.java
 
 class MainActivity : AbstractAppActivity(), AndroidScopeComponent {
 

c930eea4dc30645d6c87c6d79d58f3b12197f122
lipsa-b <lipsa.behera@sutara.org>
2026-03-19 20:08:57 +0530

    fix the homescreen

--- respect-app-compose/src/commonMain/kotlin/world/respect/app/view/home/HomeScreen.kt
+++ respect-app-compose/src/commonMain/kotlin/world/respect/app/view/home/HomeScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.SecondaryTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import kotlinx.coroutines.launch
@@ -20,10 +19,8 @@ import world.respect.app.view.playlists.mapping.list.PlaylistListScreenForViewMo
 import world.respect.app.viewmodel.respectViewModel
 import world.respect.shared.generated.resources.Res
 import world.respect.shared.generated.resources.apps
-import world.respect.shared.generated.resources.home
 import world.respect.shared.generated.resources.playlists
 import world.respect.shared.navigation.RespectComposeNavController
-import world.respect.shared.util.ext.asUiText
 import world.respect.shared.viewmodel.app.appstate.AppUiState
 import world.respect.shared.viewmodel.apps.launcher.AppLauncherViewModel
 import world.respect.shared.viewmodel.playlists.mapping.list.PlaylistListViewModel

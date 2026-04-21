40fbbd10e7db5eac88f62c3d1fd695b8f66ceaf2
Mike Dawson <mike@ustadmobile.com>
2026-04-21 13:41:07 +0400

    Move statements read into a transaction. Add support for verb search in statements.

--- respect-datalayer-db/src/commonMain/kotlin/world/respect/datalayer/db/school/xapi/XapiStatementDataSourceDb.kt
+++ respect-datalayer-db/src/commonMain/kotlin/world/respect/datalayer/db/school/xapi/XapiStatementDataSourceDb.kt
@@ -1,5 +1,7 @@
 package world.respect.datalayer.db.school.xapi
 
+import androidx.room.Transactor
+import androidx.room.useReaderConnection
 import io.ktor.http.Url
 import kotlinx.serialization.json.Json
 import world.respect.datalayer.AuthenticatedUserPrincipalId
@@ -117,102 +119,105 @@ class XapiStatementDataSourceDb(
     ): DataLoadState<List<XapiStatement>> {
 
         val statementIds = listParams.statementId?.toLongPair()
+        return schoolDb.useReaderConnection { con ->
 
-        //Get the statement entities.
-        //Then get the activity entities
-        val statements =  schoolDb.getStatementDao().list(
-            statementIdHi = statementIds?.first ?: 0,
-            statementIdLo = statementIds?.second ?: 0,
-            agentUid = listParams.agent?.identifierHash(uidNumberMapper) ?: 0,
-            relatedAgents = listParams.relatedAgents ?: false,
-            since = listParams.since?.toEpochMilliseconds() ?: XapiStatementEntityDao.SINCE_UNSET,
-            until = listParams.since?.toEpochMilliseconds() ?: XapiStatementEntityDao.UNTIL_UNSET,
-        ).map { entity ->
-            val substatementEntity = schoolDb.takeIf {
-                entity.stmtEntity.statementObjectType == XapiStatementEntityObjectTypeEnum.SUBSTATEMENT
-            }?.getStatementDao()?.getEntityForSubstatement(
-                subStatementIdHi = entity.stmtEntity.statementObjectUid1,
-                subStatementIdLo = entity.stmtEntity.statementObjectUid2,
-            )
+            con.withTransaction(Transactor.SQLiteTransactionType.DEFERRED) {
+                val statements =  schoolDb.getStatementDao().list(
+                    statementIdHi = statementIds?.first ?: 0,
+                    statementIdLo = statementIds?.second ?: 0,
+                    agentUid = listParams.agent?.identifierHash(uidNumberMapper) ?: 0,
+                    verbUid = listParams.verb?.let { uidNumberMapper(it) } ?: 0,
+                    relatedAgents = listParams.relatedAgents ?: false,
+                    since = listParams.since?.toEpochMilliseconds() ?: XapiStatementEntityDao.SINCE_UNSET,
+                    until = listParams.since?.toEpochMilliseconds() ?: XapiStatementEntityDao.UNTIL_UNSET,
+                ).map { entity ->
+                    val substatementEntity = schoolDb.takeIf {
+                        entity.stmtEntity.statementObjectType == XapiStatementEntityObjectTypeEnum.SUBSTATEMENT
+                    }?.getStatementDao()?.getEntityForSubstatement(
+                        subStatementIdHi = entity.stmtEntity.statementObjectUid1,
+                        subStatementIdLo = entity.stmtEntity.statementObjectUid2,
+                    )
 
-            //now get StatementContextActivityJoins
-            val contextActivityJoins = schoolDb.getStatementContextActivityJoinDao()
-                .findAllByStatementIds(
-                    statementIdHi = entity.stmtEntity.statementIdHi,
-                    statementIdLo = entity.stmtEntity.statementIdLo,
-                    statementIdHi2 = substatementEntity?.stmtEntity?.statementIdHi ?: 0,
-                    statementIdLo2 = substatementEntity?.stmtEntity?.statementIdLo ?: 0,
-                )
+                    //now get StatementContextActivityJoins
+                    val contextActivityJoins = schoolDb.getStatementContextActivityJoinDao()
+                        .findAllByStatementIds(
+                            statementIdHi = entity.stmtEntity.statementIdHi,
+                            statementIdLo = entity.stmtEntity.statementIdLo,
+                            statementIdHi2 = substatementEntity?.stmtEntity?.statementIdHi ?: 0,
+                            statementIdLo2 = substatementEntity?.stmtEntity?.statementIdLo ?: 0,
+                        )
 
-            val verbLangMapEntries = schoolDb.getVerbLangMapEntryDao().findByVerbUidPair(
-                uid1 = entity.verbEntity.verbUid,
-                uid2 = substatementEntity?.verbEntity?.verbUid ?: 0,
-            )
+                    val verbLangMapEntries = schoolDb.getVerbLangMapEntryDao().findByVerbUidPair(
+                        uid1 = entity.verbEntity.verbUid,
+                        uid2 = substatementEntity?.verbEntity?.verbUid ?: 0,
+                    )
 
-            val verbs = listOfNotNull(
-                entity.verbEntity, substatementEntity?.verbEntity,
-            ).map { verbEntity ->
-                VerbEntities(
-                    verbEntity = verbEntity,
-                    verbLangMapEntries = verbLangMapEntries.filter {
-                        it.vlmeVerbUid == verbEntity.verbUid
+                    val verbs = listOfNotNull(
+                        entity.verbEntity, substatementEntity?.verbEntity,
+                    ).map { verbEntity ->
+                        VerbEntities(
+                            verbEntity = verbEntity,
+                            verbLangMapEntries = verbLangMapEntries.filter {
+                                it.vlmeVerbUid == verbEntity.verbUid
+                            }
+                        ).toModel()
                     }
-                ).toModel()
-            }
 
-            val allActorUids = entity.stmtEntity.allActorUids().appendIfNotNull(
-                substatementEntity?.stmtEntity?.allActorUids()
-            )
+                    val allActorUids = entity.stmtEntity.allActorUids().appendIfNotNull(
+                        substatementEntity?.stmtEntity?.allActorUids()
+                    )
 
-            val actorEntities = schoolDb.getActorDao().findByUidList(uids = allActorUids)
-            val groupMemberJoins = schoolDb.getGroupMemberActorJoinDao().findByGroupActorUidList(
-                uidList = allActorUids
-            )
+                    val actorEntities = schoolDb.getActorDao().findByUidList(uids = allActorUids)
+                    val groupMemberJoins = schoolDb.getGroupMemberActorJoinDao().findByGroupActorUidList(
+                        uidList = allActorUids
+                    )
 
-            val actors = actorEntities.filter {
-                //Filter out agents that are referenced by being group members
-                it.actorUid in allActorUids
-            }.distinctBy {
-                it.actorUid
-            }.map { actorEntity ->
-                //Passing through all actor entitie is harmless because the ActorEntities.toModel
-                //will use the group member joins to select the actor entity for an agent member of
-                //the group
-                ActorEntities(
-                    actor = actorEntity,
-                    groupMemberJoins = groupMemberJoins.filter {
-                        it.gmajGroupActorUid == actorEntity.actorUid
-                    },
-                    groupMemberAgents = actorEntities,
-                ).toModel()
-            }
+                    val actors = actorEntities.filter {
+                        //Filter out agents that are referenced by being group members
+                        it.actorUid in allActorUids
+                    }.distinctBy {
+                        it.actorUid
+                    }.map { actorEntity ->
+                        //Passing through all actor entitie is harmless because the ActorEntities.toModel
+                        //will use the group member joins to select the actor entity for an agent member of
+                        //the group
+                        ActorEntities(
+                            actor = actorEntity,
+                            groupMemberJoins = groupMemberJoins.filter {
+                                it.gmajGroupActorUid == actorEntity.actorUid
+                            },
+                            groupMemberAgents = actorEntities,
+                        ).toModel()
+                    }
 
-            val activities = schoolDb.getActivityEntityDao().findByUidList(
-                entity.stmtEntity.allActivityUids(contextActivityJoins).appendIfNotNull(
-                    substatementEntity?.stmtEntity?.allActivityUids(contextActivityJoins)
+                    val activities = schoolDb.getActivityEntityDao().findByUidList(
+                        entity.stmtEntity.allActivityUids(contextActivityJoins).appendIfNotNull(
+                            substatementEntity?.stmtEntity?.allActivityUids(contextActivityJoins)
+                        )
+                    ).map { it.toModel(json) }
+
+                    StatementEntities(
+                        statements = listOfNotNull(
+                            entity.stmtEntity, substatementEntity?.stmtEntity
+                        ),
+                        statementEntityJson = listOf(entity.stmtJsonEntity),
+                        statementContextActivityJoins = contextActivityJoins,
+                    ).toModel(
+                        json = json,
+                        statementIdHi = entity.stmtEntity.statementIdHi,
+                        statementIdLo = entity.stmtEntity.statementIdLo,
+                        uidNumberMapper = uidNumberMapper,
+                        verbs = verbs,
+                        actors = actors,
+                        activities = activities
+                    )
+                }
+
+                DataReadyState(
+                    data = statements,
                 )
-            ).map { it.toModel(json) }
-
-            StatementEntities(
-                statements = listOfNotNull(
-                    entity.stmtEntity, substatementEntity?.stmtEntity
-                ),
-                statementEntityJson = listOf(entity.stmtJsonEntity),
-                statementContextActivityJoins = contextActivityJoins,
-            ).toModel(
-                json = json,
-                statementIdHi = entity.stmtEntity.statementIdHi,
-                statementIdLo = entity.stmtEntity.statementIdLo,
-                uidNumberMapper = uidNumberMapper,
-                verbs = verbs,
-                actors = actors,
-                activities = activities
-            )
+            }
         }
-
-        return DataReadyState(
-            data = statements,
-        )
     }
 
     override suspend fun findByUidList(uids: List<String>): List<XapiStatement> {

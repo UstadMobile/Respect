30ed5cb4935d617f6bac4fa59ffc288c19079f79
mandviverma <mandvi.verma@sutara.org>
2026-04-22 14:41:20 +0530

    Implement xAPI statement logging for student group creation in `StudentGroupingEditViewModel`.

--- respect-lib-shared/src/commonMain/kotlin/world/respect/shared/viewmodel/studentgrouping/edit/StudentGroupingEditViewModel.kt
+++ respect-lib-shared/src/commonMain/kotlin/world/respect/shared/viewmodel/studentgrouping/edit/StudentGroupingEditViewModel.kt
@@ -1,10 +1,12 @@
 package world.respect.shared.viewmodel.studentgrouping.edit
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import org.koin.core.component.KoinScopeComponent
 import org.koin.core.component.inject
 import org.koin.core.scope.Scope
@@ -14,14 +16,22 @@ import world.respect.datalayer.db.school.ext.fullName
 import world.respect.datalayer.school.PersonDataSource
 import world.respect.datalayer.school.model.EnrollmentRoleEnum
 import world.respect.datalayer.school.model.Person
+import world.respect.datalayer.school.xapi.model.VERB_COMPLETED
 import world.respect.datalayer.school.xapi.model.XapiAccount
+import world.respect.datalayer.school.xapi.model.XapiActor
 import world.respect.datalayer.school.xapi.model.XapiAgent
+import world.respect.datalayer.school.xapi.model.XapiGroup
 import world.respect.datalayer.school.xapi.model.XapiObjectType
+import world.respect.datalayer.school.xapi.model.XapiState
+import world.respect.datalayer.school.xapi.model.XapiStatement
+import world.respect.datalayer.school.xapi.model.XapiVerb
 import world.respect.datalayer.shared.paging.EmptyPagingSourceFactory
 import world.respect.datalayer.shared.paging.IPagingSourceFactory
 import world.respect.datalayer.shared.paging.PagingSourceFactoryHolder
+import world.respect.lib.serializers.InstantAsISO8601
 import world.respect.libutil.util.time.localDateInCurrentTimeZone
 import world.respect.shared.domain.account.RespectAccountManager
+import world.respect.shared.domain.account.RespectSessionAndPerson
 import world.respect.shared.generated.resources.Res
 import world.respect.shared.generated.resources.create_group
 import world.respect.shared.generated.resources.save
@@ -32,9 +42,14 @@ import world.respect.shared.resources.UiText
 import world.respect.shared.util.ext.asUiText
 import world.respect.shared.viewmodel.RespectViewModel
 import world.respect.shared.viewmodel.app.appstate.ActionBarButtonUiState
+import java.util.UUID
 import kotlin.getValue
+import kotlin.time.Clock
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 data class StudentGroupingEditUiState(
+    val selectedAccount: RespectSessionAndPerson? = null,
     val nameError: UiText? = null,
     val students: IPagingSourceFactory<Int, Person> = EmptyPagingSourceFactory(),
     val selectedStudentIds: List<String> = emptyList(),
@@ -43,9 +58,9 @@ data class StudentGroupingEditUiState(
 
 class StudentGroupingEditViewModel(
     savedStateHandle: SavedStateHandle,
-    var accountManager: RespectAccountManager,
+    var respectAccountManager: RespectAccountManager,
 ) : RespectViewModel(savedStateHandle), KoinScopeComponent {
-    override val scope: Scope = accountManager.requireActiveAccountScope()
+    override val scope: Scope = respectAccountManager.requireActiveAccountScope()
 
     private val _uiState = MutableStateFlow(StudentGroupingEditUiState())
 
@@ -89,8 +104,17 @@ class StudentGroupingEditViewModel(
         }
     }
 
+    @OptIn(ExperimentalUuidApi::class)
     fun onClickSave() {
-        val schoolSelfUrl = accountManager.activeAccount?.school?.self
+        val schoolSelfUrl = respectAccountManager.activeAccount?.school?.self
+        val groupName =""
+        viewModelScope.launch {
+            respectAccountManager.selectedAccountAndPersonFlow.collect { accountAndPerson ->
+                _uiState.update { prev ->
+                    prev.copy(selectedAccount = accountAndPerson)
+                }
+            }
+        }
         val members = _uiState.value.selectedStudentNames.map { studentName ->
             XapiAgent(
                 name = studentName,
@@ -103,6 +127,36 @@ class StudentGroupingEditViewModel(
         }
 
 
+        val group = XapiGroup(
+            objectType = XapiObjectType.Group,
+            account = XapiAccount(
+                name = groupName,
+                homePage = schoolSelfUrl.toString()
+            ),
+            member = members
+        )
+
+        val actor = XapiAgent(
+            name = _uiState.value.selectedAccount?.person?.fullName(),
+            objectType = XapiObjectType.Agent,
+            account = XapiAccount(
+                name = _uiState.value.selectedAccount?.person?.fullName()?:"",
+                homePage = schoolSelfUrl.toString()
+            )
+        )
+
+        val verb = XapiVerb(
+            id = VERB_COMPLETED
+        )
+
+        val statement= XapiStatement(
+            actor = actor,
+            verb = verb,
+            `object` = group
+        )
+        launchWithLoadingIndicator {
+            schoolDataSource.xapiStatementDataSource.store(listOf(statement))
+        }
         _navCommandFlow.tryEmit(
             NavCommand.Navigate(
                 StudentGroupingDetail

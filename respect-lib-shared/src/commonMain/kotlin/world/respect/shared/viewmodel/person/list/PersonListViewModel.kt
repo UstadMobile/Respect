93e8ed5b9f0b2414facb4a76d63f604d64d9a31b
Mike Dawson <mike@ustadmobile.com>
2026-05-06 23:15:11 +0400

    Fix for issue #231.
    When a teacher or any other is being approved, this requires permission to add the given user role. Teachers by default have permission to add students and parents, but not other teachers. They should therefor not see the option to approve a teacher registration (only admin can do that).

--- respect-lib-shared/src/commonMain/kotlin/world/respect/shared/viewmodel/person/list/PersonListViewModel.kt
+++ respect-lib-shared/src/commonMain/kotlin/world/respect/shared/viewmodel/person/list/PersonListViewModel.kt
@@ -37,7 +37,9 @@ import world.respect.shared.util.ext.asUiText
 import world.respect.shared.viewmodel.RespectViewModel
 import world.respect.shared.viewmodel.app.appstate.AppBarSearchUiState
 import world.respect.datalayer.school.domain.CheckPersonPermissionUseCase.PermissionsRequiredByRole
+import world.respect.datalayer.school.ext.writePermissionFlag
 import world.respect.datalayer.school.model.Person
+import world.respect.datalayer.school.model.PersonRoleEnum
 import world.respect.datalayer.school.model.PersonStatusEnum
 import world.respect.shared.domain.account.invite.ApproveOrDeclineInviteRequestUseCase
 import world.respect.shared.domain.permissions.CheckSchoolPermissionsUseCase
@@ -58,7 +60,15 @@ data class PersonListUiState(
     val showInvite: Boolean = false,
     val pendingPersons: IPagingSourceFactory<Int, Person> =
         IPagingSourceFactory { EmptyPagingSource() },
-)
+    val addPersonPermissions: List<Long> = emptyList(),
+) {
+
+
+    fun showApproveOption(role: PersonRoleEnum): Boolean {
+        return role.writePermissionFlag in addPersonPermissions
+    }
+
+}
 
 class PersonListViewModel(
     savedStateHandle: SavedStateHandle,
@@ -138,9 +148,11 @@ class PersonListViewModel(
         }
 
         viewModelScope.launch {
-            val canAddPerson = checkPermissionUseCase(
+            val addPermissionsAvailable = checkPermissionUseCase(
                 PermissionsRequiredByRole.WRITE_PERMISSIONS.flagList
-            ).isNotEmpty()
+            )
+            val canAddPerson = addPermissionsAvailable.isNotEmpty()
+            _uiState.update { it.copy(addPersonPermissions = addPermissionsAvailable) }
 
             val canInvitePerson = canAddPerson || route.inviteUid != null
 

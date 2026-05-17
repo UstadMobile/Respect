62ccad8b6c976ed3de4ef7ac2ef6e8e401801ced
Mike Dawson <mike@ustadmobile.com>
2026-05-17 12:13:35 +0400

    Update LaunchAppUseCaseAndroid to handle launching native app if app with a verified app link is available.
    Otherwise fall back to using our own WebView Activity. Handle SDK30+ and pre-SDK30.

--- respect-lib-shared/src/androidMain/kotlin/world/respect/shared/domain/launchapp/LaunchAppUseCaseAndroid.kt
+++ respect-lib-shared/src/androidMain/kotlin/world/respect/shared/domain/launchapp/LaunchAppUseCaseAndroid.kt
@@ -2,6 +2,11 @@ package world.respect.shared.domain.launchapp
 
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.CATEGORY_BROWSABLE
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.content.Intent.FLAG_ACTIVITY_REQUIRE_NON_BROWSER
+import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
 import androidx.core.net.toUri
 import io.github.aakira.napier.Napier
@@ -19,10 +24,22 @@ class LaunchAppUseCaseAndroid(
     private val getXapiLaunchUrlUseCase: GetXapiLaunchUrlUseCase,
 ): LaunchAppUseCase {
 
+    /**
+     * Launch a compatible app for the given LaunchRequest: if a native app with verified app links
+     * for the requested URL is installed, launch it. Otherwise, launch the requested URL in the
+     * WebView Activity (where it is possible to intercept/cache resources etc).
+     *
+     * On Android SDK30 and above:
+     *  Attempts to launch the url using an intent with FLAG_ACTIVITY_REQUIRE_NON_BROWSER as per
+     *  https://developer.android.com/training/package-visibility/use-cases#let-non-browser-apps-handle-urls
+     *
+     * On Pre-SDK30 devices:
+     *  Use the package manager to look for installed apps that can handle the intent (Pre-SDK30
+     *  restrictions on querying packages are not enforced, so this technique works).
+     */
     override suspend fun invoke(
         request: LaunchRequest
     ) {
-        val androidPackageId = null//app.android?.packageId
         val launchUrlBase = request.learningUnitId?.let {
             getXapiLaunchUrlUseCase(it)
         } ?: return
@@ -32,23 +49,42 @@ class LaunchAppUseCaseAndroid(
             )
         }.build()
 
-        Log.i("LaunchUseCase", "Launching URL: $launchUrl")
+        try {
+            val intent = Intent(
+                Intent.ACTION_VIEW, launchUrl.toString().toUri()
+            ).apply {
+                addCategory(CATEGORY_BROWSABLE)
+            }
 
-        if(androidPackageId != null) {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            intent.setPackage(androidPackageId)
-            intent.setData(launchUrl.toString().toUri())
-
-            try {
+            if(Build.VERSION.SDK_INT >= 30) {
+                intent.flags = FLAG_ACTIVITY_REQUIRE_NON_BROWSER or FLAG_ACTIVITY_NEW_TASK
+                Napier.d(
+                    "LaunchAppUseCaseAndroid: attempting to launch ${request.learningUnitId} with RequireNonBrowser"
+                )
                 appContext.startActivity(intent)
                 return
-            }catch(e: Throwable) {
-                e.printStackTrace()
-                //In future - apps will need an action we can query to check if it is installed.
-                //didn't work - maybe wasn't installed.
+            }else {
+                intent.flags = FLAG_ACTIVITY_NEW_TASK
+                val resolvedInfo = appContext.packageManager.queryIntentActivities(
+                    intent, PackageManager.MATCH_ALL
+                )
+
+                val availableNativePackages = resolvedInfo.mapNotNull {
+                    it.activityInfo.packageName
+                }.filterNot { it in KNOWN_BROWSER_PACKAGES }
+
+                Napier.d("LaunchAppUseCaseAndroid: available native packages: $availableNativePackages")
+
+                if(availableNativePackages.isNotEmpty()) {
+                    appContext.startActivity(intent)
+                    return
+                }
             }
+        }catch(e: Throwable) {
+            Napier.w("Something wrong opening learning unit through app, fallback", e)
         }
+
+        Log.i("LaunchUseCase", "Launching URL: $launchUrl")
 
         /*
          * The ActivityClass, because it's UI, is contained within the respect-app-compose module,
@@ -71,6 +107,14 @@ class LaunchAppUseCaseAndroid(
         private const val WEBVIEW_ACTIVITY_NAME = "world.respect.WebViewActivity"
 
         const val EXTRA_URL = "url"
+
+        private val KNOWN_BROWSER_PACKAGES = listOf(
+            "org.chromium.webview_shell",//WebView on emulator
+            "com.android.chrome",
+            "org.mozilla.firefox",
+            "com.sec.android.app.sbrowser", //Samsung browser
+            "com.mi.globalbrowser",//Mi browser
+        )
 
     }
 }

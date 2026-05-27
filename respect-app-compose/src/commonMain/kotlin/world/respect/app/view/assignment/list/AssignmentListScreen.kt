fde89bedc72b3d20acd920d547aaabbb8a04f223
Mike Dawson <mike@ustadmobile.com>
2026-05-27 11:42:55 +0400

    Put in empty state message for assignment and class list screens.

--- respect-app-compose/src/commonMain/kotlin/world/respect/app/view/assignment/list/AssignmentListScreen.kt
+++ respect-app-compose/src/commonMain/kotlin/world/respect/app/view/assignment/list/AssignmentListScreen.kt
@@ -1,24 +1,19 @@
 package world.respect.app.view.assignment.list
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.FilterChip
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import world.respect.app.components.RespectEmptyListComponent
 import world.respect.app.components.defaultItemPadding
+import world.respect.lib.dataloadstate.DataLoadingState
 import world.respect.lib.dataloadstate.ext.dataOrNull
 import world.respect.lib.xapi.model.AssignmentSummary
-import world.respect.shared.util.AssignmentListScreenFilter
 import world.respect.shared.viewmodel.assignment.list.AssignmentListUiState
 import world.respect.shared.viewmodel.assignment.list.AssignmentListViewModel
 
@@ -30,7 +25,6 @@ fun AssignmentListScreen(
     val uiState by viewModel.uiState.collectAsState()
     AssignmentListScreen(
         uiState = uiState,
-        onFilterSelected = viewModel::onFilterChanged,
         onClickAssignment = viewModel::onClickAssignment,
     )
 }
@@ -38,27 +32,11 @@ fun AssignmentListScreen(
 @Composable
 fun AssignmentListScreen(
     uiState: AssignmentListUiState,
-    onFilterSelected: (AssignmentListScreenFilter) -> Unit,
     onClickAssignment: (AssignmentSummary) -> Unit = { },
 ) {
     val assignments = uiState.assignments.dataOrNull() ?: emptyList()
 
     Column(modifier = Modifier.fillMaxSize()) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .defaultItemPadding(),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            AssignmentListScreenFilter.entries.forEach { filter ->
-                FilterChip(
-                    selected = uiState.selectedFilter == filter,
-                    onClick = { onFilterSelected(filter) },
-                    label = { Text(uiState.getLabelForFilter(filter)) },
-                    shape = RoundedCornerShape(50)
-                )
-            }
-        }
         LazyColumn(modifier = Modifier.fillMaxSize()) {
             items(
                 items = assignments,
@@ -67,6 +45,24 @@ fun AssignmentListScreen(
                     summary = summary,
                     onClick = onClickAssignment,
                 )
+            }
+
+            when {
+                uiState.assignments is DataLoadingState<*> -> {
+
+                }
+
+                assignments.isEmpty() -> {
+                    item("emptyitem") {
+                        RespectEmptyListComponent(
+                            modifier = Modifier.fillMaxWidth().defaultItemPadding()
+                        )
+                    }
+                }
+
+                else -> {
+                    //do nothing
+                }
             }
         }
     }

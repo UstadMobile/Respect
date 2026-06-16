97778f354e1522899876e6203591a3ea9ca58920
Anugraha <anugraha@sutara.org>
2026-06-16 11:07:20 +0530

    refactor

--- respect-lib-shared/src/commonMain/kotlin/world/respect/shared/util/DurationExt.kt
+++ respect-lib-shared/src/commonMain/kotlin/world/respect/shared/util/DurationExt.kt
@@ -1,12 +1,15 @@
 package world.respect.shared.util
 
+import world.respect.shared.generated.resources.Res
+import world.respect.shared.generated.resources.duration_hours_short
+import world.respect.shared.generated.resources.duration_minutes_short
 import kotlin.time.Duration
 
 fun Duration.formatToShortString(): String {
     return toComponents { hours, minutes, seconds, _ ->
         buildString {
-            if (hours > 0) append("${hours}h:")
-            append("${minutes}m:${seconds.toString().padStart(2, '0')}")
+            if (hours > 0) append("$hours ${Res.string.duration_hours_short}:")
+            append("${minutes}${Res.string.duration_minutes_short}:${seconds.toString().padStart(2, '0')}")
         }
     }
 }

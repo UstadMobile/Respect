a38a4626ff7043b9b7b340a31de0522ac6cb8a52
pooja <pooja@ustadmobile.com>
2026-06-29 11:51:08 +0400

    Update `ci-run-local-test.sh` and the Maestro passkey test configuration to streamline artifact handling and recording paths.
    - Modify `ci-run-local-test.sh` to update the Maestro report output path, move the generated test video to the build directory, and re-enable APK installation and Maestro test execution.
    - Update `user_signup_using_passkey_test.yaml` to simplify the `startRecording` path.
    - Refactor indentation and variable assignments in `ci-run-local-test.sh` for better consistency.

--- ci-run-local-test.sh
+++ ci-run-local-test.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 
 # Remove old build
-  rm -rf build
+   rm -rf build
 
 # Create directory - build
-  mkdir -p build
+   mkdir -p build
 
-  touch build/test_vedio.mp4
-  touch build/report.xml
-  touch build/maestro-uploaded.txt
+   touch build/maestro-uploaded.txt
 
-  JENKINS_VAR_PATH=$1
-  VAR_FILE_PATH=build/test_variables.txt
-  TEST_VIDEO_PATH=build/test_vedio.mp4
-  TEST_REPORT_PATH=build/report.xml
-  DONE_FLAG_FILE_PATH=build/maestro-uploaded.txt
+   JENKINS_VAR_PATH=$1
+   VAR_FILE_PATH=build/test_variables.txt
 
-  echo "Saving Variable file from Jenkins to local"
-  scp $JENKINS_VAR_PATH $VAR_FILE_PATH
-  source $VAR_FILE_PATH
+   echo "Saving Variable file from Jenkins to local"
+   scp $JENKINS_VAR_PATH $VAR_FILE_PATH
+   source $VAR_FILE_PATH
 
-  echo "Saving APK file from Jenkins to local"
-  scp $APP_PATH build
+   echo "Saving APK file from Jenkins to local"
+   scp $APP_PATH build
 
-  adb uninstall world.respect.app
-  adb install build/respect-app-compose-release.apk
-
+   adb uninstall world.respect.app
+   adb install build/respect-app-compose-release.apk
 
    maestro test \
       --env DIR_ADMIN_AUTH_PASS=$DIR_ADMIN_AUTH_PASS \
@@ -34,24 +28,28 @@
       --env DIR_ADMIN_AUTH_HEADER="$DIR_ADMIN_AUTH_HEADER" \
       --env SCHOOL_NAME=TestSchool \
       --format=junit \
-      --output=build/maestro/output/report.xml \
+      --output=build/report.xml \
       .maestro/flows/flow-passkey/*.yaml
       MAESTRO_STATUS=$?
 
-      TEST_VIDEO_PATH=build/user_signup_using_passkey_test.mp4
+   mv user_signup_using_passkey_test.mp4 build/
+
+   TEST_VIDEO_PATH=build/user_signup_using_passkey_test.mp4
+   TEST_REPORT_PATH=build/report.xml
+   DONE_FLAG_FILE_PATH=build/maestro-uploaded.txt
 
 # Copy the video file and report file to Jenkins
-      echo "Saving Test files from local to Jenkins"
-      scp $TEST_VIDEO_PATH $JENKINS_TEST_PATH
-      scp $TEST_REPORT_PATH $JENKINS_TEST_PATH
+   echo "Saving Test files from local to Jenkins"
+   scp $TEST_VIDEO_PATH $JENKINS_TEST_PATH
+   scp $TEST_REPORT_PATH $JENKINS_TEST_PATH
 
 # Write the status to the file
-      echo $MAESTRO_STATUS > $DONE_FLAG_FILE_PATH
+   echo $MAESTRO_STATUS > $DONE_FLAG_FILE_PATH
 
-      echo "Saving done flag to Jenkins"
-      scp $DONE_FLAG_FILE_PATH $DONE_FLAG_TEST_PATH
+   echo "Saving done flag to Jenkins"
+   scp $DONE_FLAG_FILE_PATH $DONE_FLAG_TEST_PATH
 
-      exit $MAESTRO_STATUS
+   exit $MAESTRO_STATUS
 
 
 

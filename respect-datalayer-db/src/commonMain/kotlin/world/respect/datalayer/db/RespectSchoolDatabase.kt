74c59e18bea51c8047b0ccaf75faf64bcc521b00
mandviverma <mandvi.verma@sutara.org>
2026-06-30 13:43:58 +0530

    Update `002_browse_lessons_test.yaml` to use `${output.TEST_APP_URL}` variable.

--- respect-datalayer-db/src/commonMain/kotlin/world/respect/datalayer/db/RespectSchoolDatabase.kt
+++ respect-datalayer-db/src/commonMain/kotlin/world/respect/datalayer/db/RespectSchoolDatabase.kt
@@ -35,7 +35,6 @@ import world.respect.datalayer.db.shared.entities.LangMapEntity
 import world.respect.datalayer.db.school.daos.IndicatorEntityDao
 import world.respect.datalayer.db.school.daos.ReportEntityDao
 import world.respect.datalayer.db.realm.entities.IndicatorEntity
-import world.respect.datalayer.db.school.daos.BookmarkDao
 import world.respect.datalayer.db.school.daos.ClassEntityDao
 import world.respect.datalayer.db.school.daos.ClassPermissionEntityDao
 import world.respect.datalayer.db.school.daos.EnrollmentEntityDao
@@ -55,7 +54,6 @@ import world.respect.datalayer.db.school.entities.ReportEntity
 import world.respect.datalayer.db.school.entities.SchoolAppEntity
 import world.respect.datalayer.db.school.entities.WriteQueueItemEntity
 import world.respect.datalayer.db.school.daos.SchoolPermissionGrantDao
-import world.respect.datalayer.db.school.entities.BookmarkEntity
 import world.respect.datalayer.db.school.entities.ClassPermissionEntity
 import world.respect.datalayer.db.school.entities.PullSyncStatusEntity
 import world.respect.datalayer.db.school.entities.SchoolPermissionGrantEntity
@@ -112,7 +110,6 @@ import world.respect.datalayer.school.model.Report
         PullSyncStatusEntity::class,
         PersonQrBadgeEntity::class,
         InviteEntity::class,
-        BookmarkEntity::class,
 
         //Shared (used by OPDS)
         LangMapEntity::class,
@@ -180,8 +177,6 @@ abstract class RespectSchoolDatabase: RoomDatabase() {
 
     abstract fun getPullSyncStatusEntityDao(): PullSyncStatusEntityDao
 
-    abstract fun getBookmarkDao(): BookmarkDao
-
     abstract fun getLangMapEntityDao(): LangMapEntityDao
 
     abstract fun getOpdsFeedEntityDao(): OpdsFeedEntityDao
@@ -234,7 +229,6 @@ abstract class RespectSchoolDatabase: RoomDatabase() {
             OpdsFacetEntity.TABLE_ID,
             OpdsGroupEntity.TABLE_ID,
             OpdsFeedEntity.TABLE_ID,
-            BookmarkEntity.TABLE_ID
         )
 
     }

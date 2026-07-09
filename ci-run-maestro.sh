0407d02587afa5595bffe0be3774d4f56ade5c8b
pooja <pooja@ustadmobile.com>
2026-07-09 10:57:03 +0400

    Hardcode Maestro Cloud device OS to "android-35" and disable dynamic `DEVICE_OS` assignment in `ci-run-maestro.sh`.

--- ci-run-maestro.sh
+++ ci-run-maestro.sh
@@ -150,12 +150,12 @@ if [ "$1" == "cloud" ]; then
         PULLREQUEST_ARG="--pull-request-id=$PULLREQUEST"
     fi
 
-    if [ "$DEVICE_OS" == "" ]; then
-        DEVICE_OS="android-35"
-    fi
+#    if [ "$DEVICE_OS" == "" ]; then
+#        DEVICE_OS="android-35"
+#    fi
 
     maestro cloud \
-        --device-os "$DEVICE_OS" \
+        --device-os "android-35" \
         --api-key=$MAESTRO_CLOUD_APIKEY \
         --project-id=$MAESTRO_CLOUD_PROJECTID \
         --app-file=./respect-app-compose/build/outputs/apk/release/respect-app-compose-release.apk \

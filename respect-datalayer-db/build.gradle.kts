82241844fac122cf490228c6cdd1b4c4f8c8d97b
Mike Dawson <mike@ustadmobile.com>
2026-09-10 12:10:36 +0400

    Remove gradle properties put there by previous AGP plugin upgrade assistants that are not needed.
    Remove most usage of deprecated by getting.

--- respect-datalayer-db/build.gradle.kts
+++ respect-datalayer-db/build.gradle.kts
@@ -54,10 +54,8 @@ kotlin {
             implementation(projects.respectLibTestResources)
         }
 
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
         }
     }
 }
--- respect-datalayer-http/build.gradle.kts
+++ respect-datalayer-http/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
@@ -41,10 +40,8 @@ kotlin {
 
         }
 
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
         }
     }
 }

660d9c447d17a594d894a0ae82299feb2c23cbf7
Mike Dawson <mike@ustadmobile.com>
2026-09-16 21:19:02 +0400

    Initial working implementation of XapiActivityProfileResourceRepository.

--- lib-datalayer-repository/src/jvmTest/kotlin/world/respect/datalayer/repository/school/xapi/XapiActivityProfileResourceRepositoryTest.kt
+++ lib-datalayer-repository/src/jvmTest/kotlin/world/respect/datalayer/repository/school/xapi/XapiActivityProfileResourceRepositoryTest.kt
@@ -1,13 +1,24 @@
 package world.respect.datalayer.repository.school.xapi
 
+import app.cash.turbine.test
+import io.ktor.server.routing.route
+import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 import org.openeel.libxapi.test.AbstractXapiActivityProfileResourceTest
+import org.openeel.libxapi.test.XapiActivityProfileTestParams
 import world.respect.datalayer.http.server.XapiActivityProfileResourceRoute
+import world.respect.lib.dataloadstate.DataLoadParams
+import world.respect.lib.dataloadstate.DataReadyState
+import world.respect.lib.xapi.model.XapiDocument
 import world.respect.lib.xapi.resources.XapiActivityProfileResource
 import kotlin.test.Test
+import kotlin.test.assertIs
+import kotlin.test.assertNotNull
+import kotlin.time.Duration.Companion.seconds
 
-/*
+
 class XapiActivityProfileResourceRepositoryTest: AbstractXapiActivityProfileResourceTest() {
 
     @JvmField
@@ -17,7 +28,7 @@ class XapiActivityProfileResourceRepositoryTest: AbstractXapiActivityProfileReso
     override suspend fun withXapiActivityProfileResource(
         block: suspend (XapiActivityProfileResource) -> Unit
     ) {
-        withEmbeddedServerAndRepositoryResources(
+        withEmbeddedServerAndRepositoryClients(
             workDir = temporaryFolder.newFolder(),
             routingConfig = { serverContext ->
                 XapiActivityProfileResourceRoute(
@@ -32,14 +43,60 @@ class XapiActivityProfileResourceRepositoryTest: AbstractXapiActivityProfileReso
     }
 
     @Test
-    fun test() {
-        givenNonExistentDocument_whenPosted_thenCreatesNewDocument()
+    fun givenDocumentPostedOnClient_whenGetFlowOnServerCollected_thenIsCollected() = runBlocking {
+        withEmbeddedServerAndRepositoryClients(
+            workDir = temporaryFolder.newFolder(),
+            routingConfig = { serverContext ->
+                route("activities") {
+                    XapiActivityProfileResourceRoute(
+                        activityProfileResource = {
+                            serverContext.datasourceContext.datasource.xapiResource.activityProfile
+                        }
+                    )
+                }
+            }
+        ) {
+            val params = XapiActivityProfileTestParams.SINGLE_DOC_PARAMS1
+
+            clients.first().datasource.activityProfile.post(
+                params = params,
+                document = XapiActivityProfileTestParams.DOC
+            )
+
+            serverContext.datasourceContext.datasource.xapiResource.activityProfile.getAsFlow(
+                params = params,
+                dataLoadParams = DataLoadParams()
+            ).mapNotNull {
+                it as? DataReadyState
+            }.test(timeout = 5.seconds) {
+                assertNotNull(awaitItem().data)
+            }
+        }
     }
 
     @Test
-    fun test2() {
-        givenExistingJsonDocument_whenPosted_thenMergesTopLevelProperties()
+    fun givenDocumentedPostedOnServer_whenGetCalledOnClient_thenMatches() = runBlocking {
+        withEmbeddedServerAndRepositoryClients(
+            workDir = temporaryFolder.newFolder(),
+            routingConfig = { serverContext ->
+                route("activities") {
+                    XapiActivityProfileResourceRoute(
+                        activityProfileResource = {
+                            serverContext.datasourceContext.datasource.xapiResource.activityProfile
+                        }
+                    )
+                }
+            }
+        ) {
+            val params = XapiActivityProfileTestParams.SINGLE_DOC_PARAMS1
+
+            serverContext.datasourceContext.datasource.xapiResource.activityProfile.post(
+                params = params, document = XapiActivityProfileTestParams.DOC
+            )
+
+            val clientDocLoadState = clients.first().datasource.activityProfile.get(params = params)
+            assertIs<DataReadyState<XapiDocument>>(clientDocLoadState)
+        }
     }
 
 }
-*/
--- lib-xapi-core/src/commonMain/kotlin/world/respect/lib/xapi/remotewritequeue/DrainXapiRemoteWriteQueueUseCase.kt
+++ lib-xapi-core/src/commonMain/kotlin/world/respect/lib/xapi/remotewritequeue/DrainXapiRemoteWriteQueueUseCase.kt
@@ -1,6 +1,8 @@
 package world.respect.lib.xapi.remotewritequeue
 
+import io.ktor.http.parseQueryString
 import world.respect.lib.dataloadstate.ext.dataOrNull
+import world.respect.lib.xapi.resources.XapiActivityProfileResource
 import world.respect.lib.xapi.resources.XapiResource
 import world.respect.lib.xapi.resources.XapiStatementsResource
 import world.respect.lib.xapi.resources.local.XapiResourceLocal
@@ -36,7 +38,26 @@ class DrainXapiRemoteWriteQueueUseCase(
                 }
 
                 XapiRemoteWriteQueueItem.Resource.ACTIVITY_PROFILE -> {
+                    val params = XapiActivityProfileResource.SingleDocumentParams.fromParams(
+                        parseQueryString(queueItem.itemId)
+                    )
+                    val document = localDataSource.activityProfile.get(params).dataOrNull() ?: continue
 
+                    when(queueItem.method) {
+                        XapiRemoteWriteQueueItem.Method.POST -> {
+                            remoteDataSource.activityProfile.post(params, document)
+                        }
+
+                        XapiRemoteWriteQueueItem.Method.PUT -> {
+                            remoteDataSource.activityProfile.post(params, document)
+                        }
+
+                        XapiRemoteWriteQueueItem.Method.DELETE -> {
+                            //not implemented yet
+                        }
+                    }
+
+                    xapiRemoteWriteQueue.markSent(listOf(queueItem.xrqItemId))
                 }
             }
 

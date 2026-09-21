50137ed31af5059ca8f3d9eaa165a03d50f51eab
Mike Dawson <mike@ustadmobile.com>
2026-09-21 21:34:38 +0400

    Extract AbstractXapiDocumentResourceRepositoryIntegrationTest base test class

--- lib-datalayer-repository/src/jvmTest/kotlin/world/respect/datalayer/repository/school/xapi/XapiActivityProfileResourceRepositoryTest.kt
+++ lib-datalayer-repository/src/jvmTest/kotlin/world/respect/datalayer/repository/school/xapi/XapiActivityProfileResourceRepositoryTest.kt
@@ -1,24 +1,10 @@
 package world.respect.datalayer.repository.school.xapi
 
-import app.cash.turbine.test
-import io.ktor.server.routing.route
-import kotlinx.coroutines.flow.filterIsInstance
-import kotlinx.coroutines.flow.mapNotNull
-import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 import org.openeel.libxapi.test.AbstractXapiActivityProfileResourceTest
-import org.openeel.libxapi.test.XapiActivityProfileTestParams
 import world.respect.datalayer.http.server.XapiActivityProfileResourceRoute
-import world.respect.lib.dataloadstate.DataLoadParams
-import world.respect.lib.dataloadstate.DataReadyState
-import world.respect.lib.xapi.model.XapiDocument
 import world.respect.lib.xapi.resources.XapiActivityProfileResource
-import kotlin.test.Test
-import kotlin.test.assertContentEquals
-import kotlin.test.assertIs
-import kotlin.test.assertNotNull
-import kotlin.time.Duration.Companion.seconds
 
 
 class XapiActivityProfileResourceRepositoryTest: AbstractXapiActivityProfileResourceTest() {
